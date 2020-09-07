470cb0486695d2c3e8507e6a418258a2af2038b3
Jussi <jussi@nevavuori.fi>
2020-09-07 16:58:44 +0300

    Updates

---
+++ precache-manifest.d98da392d8ca2f732789e98fcb00cce3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f559b16b6975661ca23f3db3c8666f46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cc9dd0867a857aabc9a",
+    "url": "/static/css/main.b8ddcf63.chunk.css"
+  },
+  {
+    "revision": "101a3d28860aea367bf6",
+    "url": "/static/js/2.8cac7acc.chunk.js"
+  },
+  {
+    "revision": "d0285ca94b31c653602dcfd0b67444a0",
+    "url": "/static/js/2.8cac7acc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cc9dd0867a857aabc9a",
+    "url": "/static/js/main.c8cde506.chunk.js"
+  },
+  {
+    "revision": "9617a3acffe0718a9f16",
+    "url": "/static/js/runtime-main.d51d26c1.js"
+  },
+  {
+    "revision": "b0effcc29fe3384a4aecad96dfd4e15a",
+    "url": "/static/media/awar-meman-Ujt8Ti-5OQ0-unsplash.b0effcc2.jpg"
+  },
+  {
+    "revision": "696b80ecd6e2420e4b3b0c8167b33b6f",
+    "url": "/static/media/logo_google.696b80ec.png"
+  },
+  {
+    "revision": "afe789093ee5e4e7a6fc6eb36778955a",
+    "url": "/static/media/pexels-johannes-plenio-1103970.afe78909.jpg"
+  }
+]);
