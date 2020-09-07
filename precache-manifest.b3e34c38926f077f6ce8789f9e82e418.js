2ea221af4903008c950d9f39a7f7784518855289
Jussi <jussi@nevavuori.fi>
2020-09-07 17:04:45 +0300

    Updates

---
+++ precache-manifest.b3e34c38926f077f6ce8789f9e82e418.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af082a15f15630d81483d9363bc9e63",
+    "url": "/expence-web/index.html"
+  },
+  {
+    "revision": "24dc9f83ee761ffb8502",
+    "url": "/expence-web/static/css/main.fe6d17a1.chunk.css"
+  },
+  {
+    "revision": "7c98eaa1d5fffa34fd84",
+    "url": "/expence-web/static/js/2.707bbc09.chunk.js"
+  },
+  {
+    "revision": "d0285ca94b31c653602dcfd0b67444a0",
+    "url": "/expence-web/static/js/2.707bbc09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24dc9f83ee761ffb8502",
+    "url": "/expence-web/static/js/main.cc0d7969.chunk.js"
+  },
+  {
+    "revision": "ce34a8c1b0564ea0a0e4",
+    "url": "/expence-web/static/js/runtime-main.cd90f0eb.js"
+  },
+  {
+    "revision": "b0effcc29fe3384a4aecad96dfd4e15a",
+    "url": "/expence-web/static/media/awar-meman-Ujt8Ti-5OQ0-unsplash.b0effcc2.jpg"
+  },
+  {
+    "revision": "696b80ecd6e2420e4b3b0c8167b33b6f",
+    "url": "/expence-web/static/media/logo_google.696b80ec.png"
+  },
+  {
+    "revision": "afe789093ee5e4e7a6fc6eb36778955a",
+    "url": "/expence-web/static/media/pexels-johannes-plenio-1103970.afe78909.jpg"
+  }
+]);
